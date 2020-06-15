881cb683e9f9c06fd5129aa15229b8aa5079b4ad
Aleksefo <aleksefo@gmail.com>
2020-06-15 20:04:28 +0300

    Add example jpeg and webp images to App.js

--- example/App.js
+++ example/App.js
@@ -10,80 +10,50 @@ import React from 'react';
 import {
   SafeAreaView,
   StyleSheet,
-  ScrollView,
-  View,
   Text,
   StatusBar,
+  Image,
+  ImageBackground,
 } from 'react-native';
 
-import {
-  Header,
-  LearnMoreLinks,
-  Colors,
-  DebugInstructions,
-  ReloadInstructions,
-} from 'react-native/Libraries/NewAppScreen';
+import {Colors} from 'react-native/Libraries/NewAppScreen';
 
 const App: () => React$Node = () => {
   return (
     <>
       <StatusBar barStyle="dark-content" />
-      <SafeAreaView>
-        <ScrollView
-          contentInsetAdjustmentBehavior="automatic"
-          style={styles.scrollView}>
-          <Header />
-          {global.HermesInternal == null ? null : (
-            <View style={styles.engine}>
-              <Text style={styles.footer}>Engine: Hermes</Text>
-            </View>
-          )}
-          <View style={styles.body}>
-            <View style={styles.sectionContainer}>
-              <Text style={styles.sectionTitle}>Step One</Text>
-              <Text style={styles.sectionDescription}>
-                Edit <Text style={styles.highlight}>App.js</Text> to change this
-                screen and then come back to see your edits.
-              </Text>
-            </View>
-            <View style={styles.sectionContainer}>
-              <Text style={styles.sectionTitle}>See Your Changes</Text>
-              <Text style={styles.sectionDescription}>
-                <ReloadInstructions />
-              </Text>
-            </View>
-            <View style={styles.sectionContainer}>
-              <Text style={styles.sectionTitle}>Debug</Text>
-              <Text style={styles.sectionDescription}>
-                <DebugInstructions />
-              </Text>
-            </View>
-            <View style={styles.sectionContainer}>
-              <Text style={styles.sectionTitle}>Learn More</Text>
-              <Text style={styles.sectionDescription}>
-                Read the docs to discover what to do next:
-              </Text>
-            </View>
-            <LearnMoreLinks />
-          </View>
-        </ScrollView>
+      <SafeAreaView style={styles.container}>
+        <Text style={styles.sectionTitle}>RN WebP example</Text>
+        <Text>WebP Image</Text>
+        <Image
+          source={require('./assets/1.sm.webp')}
+          style={{height: 150, width: '100%'}}
+        />
+        <Text>Jpeg Image</Text>
+        <Image
+          source={require('./assets/1.sm.jpg')}
+          style={{height: 150, width: '100%'}}
+        />
+        <Text>WebP ImageBackground</Text>
+        <ImageBackground
+          source={require('./assets/3.sm.webp')}
+          style={{
+            height: 150,
+            width: '100%',
+            resizeMode: 'cover',
+            justifyContent: 'center',
+          }}>
+          <Text style={styles.description}>Text inside of the image</Text>
+        </ImageBackground>
       </SafeAreaView>
     </>
   );
 };
 
 const styles = StyleSheet.create({
-  scrollView: {
-    backgroundColor: Colors.lighter,
-  },
-  engine: {
-    position: 'absolute',
-    right: 0,
-  },
-  body: {
+  container: {
     backgroundColor: Colors.white,
-  },
-  sectionContainer: {
+    flex: 1,
     marginTop: 32,
     paddingHorizontal: 24,
   },
@@ -92,22 +62,10 @@ const styles = StyleSheet.create({
     fontWeight: '600',
     color: Colors.black,
   },
-  sectionDescription: {
-    marginTop: 8,
-    fontSize: 18,
-    fontWeight: '400',
-    color: Colors.dark,
-  },
-  highlight: {
-    fontWeight: '700',
-  },
-  footer: {
-    color: Colors.dark,
-    fontSize: 12,
+  description: {
+    fontSize: 24,
     fontWeight: '600',
-    padding: 4,
-    paddingRight: 12,
-    textAlign: 'right',
+    color: Colors.white,
   },
 });
 
