86e93988d9290bab627c31c031c67790f4553244
Alex Fomushkin <aleksefo@gmail.com>
2025-03-05 22:47:50 +0100

    Update Example to the latest RN version 0.78.0

--- example/App.tsx
+++ example/App.tsx
@@ -1,61 +1,57 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- */
-
 import React from 'react';
 import {
+  Image,
+  ImageBackground,
   SafeAreaView,
   StatusBar,
   StyleSheet,
   Text,
   useColorScheme,
-  Image,
-  ImageBackground,
+  View,
 } from 'react-native';
-
 import {Colors} from 'react-native/Libraries/NewAppScreen';
 
-function App(): JSX.Element {
+function App(): React.JSX.Element {
   const isDarkMode = useColorScheme() === 'dark';
 
   const backgroundStyle = {
     backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
   };
-  const {width, height} = Image.resolveAssetSource(
-    require('./assets/1.sm.webp'),
-  );
-  console.log('App2, ', {width, height});
+
   return (
-    <SafeAreaView style={backgroundStyle}>
+    <View style={backgroundStyle}>
       <StatusBar
         barStyle={isDarkMode ? 'light-content' : 'dark-content'}
         backgroundColor={backgroundStyle.backgroundColor}
       />
-      <Text style={styles.title}>RN WebP example</Text>
-      <Text>WebP Image</Text>
-      <Image source={require('./assets/1.sm.webp')} style={styles.image} />
-      <Text>Animated WebP Image</Text>
-      <Image
-        source={require('./assets/animated-webp-supported.webp')}
-        style={styles.image}
-      />
-      <Text>WebP Image from a website</Text>
-      <Image
-        source={{
-          uri: 'https://www.gstatic.com/webp/gallery/2.jpg',
-        }}
-        style={styles.image}
-      />
-      <Text>WebP ImageBackground</Text>
-      <ImageBackground
-        source={require('./assets/3.sm.webp')}
-        style={styles.imageBackground}>
-        <Text style={styles.description}>Text inside of the image</Text>
-      </ImageBackground>
-    </SafeAreaView>
+      <SafeAreaView style={backgroundStyle}>
+        <StatusBar
+          barStyle={isDarkMode ? 'light-content' : 'dark-content'}
+          backgroundColor={backgroundStyle.backgroundColor}
+        />
+        <Text style={styles.title}>RN WebP example</Text>
+        <Text>WebP Image</Text>
+        <Image source={require('./assets/1.sm.webp')} style={styles.image} />
+        <Text>Animated WebP Image</Text>
+        <Image
+          source={require('./assets/animated-webp-supported.webp')}
+          style={styles.image}
+        />
+        <Text>WebP Image from a website</Text>
+        <Image
+          source={{
+            uri: 'https://www.gstatic.com/webp/gallery/2.jpg',
+          }}
+          style={styles.image}
+        />
+        <Text>WebP ImageBackground</Text>
+        <ImageBackground
+          source={require('./assets/3.sm.webp')}
+          style={styles.imageBackground}>
+          <Text style={styles.description}>Text inside of the image</Text>
+        </ImageBackground>
+      </SafeAreaView>
+    </View>
   );
 }
 
@@ -63,6 +59,7 @@ const styles = StyleSheet.create({
   title: {
     fontSize: 24,
     fontWeight: '600',
+    alignSelf: 'center',
   },
   image: {
     height: '20%',
--- example/android/app/src/main/java/com/example/MainApplication.kt
+++ example/android/app/src/main/java/com/example/MainApplication.kt
@@ -1,62 +1,44 @@
-package com.example;
+package com.example
 
-import android.app.Application;
-import com.facebook.react.PackageList;
-import com.facebook.react.ReactApplication;
-import com.facebook.react.ReactNativeHost;
-import com.facebook.react.ReactPackage;
-import com.facebook.react.defaults.DefaultNewArchitectureEntryPoint;
-import com.facebook.react.defaults.DefaultReactNativeHost;
-import com.facebook.soloader.SoLoader;
-import java.util.List;
+import android.app.Application
+import com.facebook.react.PackageList
+import com.facebook.react.ReactApplication
+import com.facebook.react.ReactHost
+import com.facebook.react.ReactNativeHost
+import com.facebook.react.ReactPackage
+import com.facebook.react.defaults.DefaultNewArchitectureEntryPoint.load
+import com.facebook.react.defaults.DefaultReactHost.getDefaultReactHost
+import com.facebook.react.defaults.DefaultReactNativeHost
+import com.facebook.react.soloader.OpenSourceMergedSoMapping
+import com.facebook.soloader.SoLoader
 
-public class MainApplication extends Application implements ReactApplication {
+class MainApplication : Application(), ReactApplication {
 
-  private final ReactNativeHost mReactNativeHost =
-      new DefaultReactNativeHost(this) {
-        @Override
-        public boolean getUseDeveloperSupport() {
-          return BuildConfig.DEBUG;
-        }
+  override val reactNativeHost: ReactNativeHost =
+      object : DefaultReactNativeHost(this) {
+        override fun getPackages(): List<ReactPackage> =
+            PackageList(this).packages.apply {
+              // Packages that cannot be autolinked yet can be added manually here, for example:
+              // add(MyReactNativePackage())
+            }
 
-        @Override
-        protected List<ReactPackage> getPackages() {
-          @SuppressWarnings("UnnecessaryLocalVariable")
-          List<ReactPackage> packages = new PackageList(this).getPackages();
-          // Packages that cannot be autolinked yet can be added manually here, for example:
-          // packages.add(new MyReactNativePackage());
-          return packages;
-        }
+        override fun getJSMainModuleName(): String = "index"
 
-        @Override
-        protected String getJSMainModuleName() {
-          return "index";
-        }
+        override fun getUseDeveloperSupport(): Boolean = BuildConfig.DEBUG
 
-        @Override
-        protected boolean isNewArchEnabled() {
-          return BuildConfig.IS_NEW_ARCHITECTURE_ENABLED;
-        }
+        override val isNewArchEnabled: Boolean = BuildConfig.IS_NEW_ARCHITECTURE_ENABLED
+        override val isHermesEnabled: Boolean = BuildConfig.IS_HERMES_ENABLED
+      }
 
-        @Override
-        protected Boolean isHermesEnabled() {
-          return BuildConfig.IS_HERMES_ENABLED;
-        }
-      };
+  override val reactHost: ReactHost
+    get() = getDefaultReactHost(applicationContext, reactNativeHost)
 
-  @Override
-  public ReactNativeHost getReactNativeHost() {
-    return mReactNativeHost;
-  }
-
-  @Override
-  public void onCreate() {
-    super.onCreate();
-    SoLoader.init(this, /* native exopackage */ false);
+  override fun onCreate() {
+    super.onCreate()
+    SoLoader.init(this, OpenSourceMergedSoMapping)
     if (BuildConfig.IS_NEW_ARCHITECTURE_ENABLED) {
       // If you opted-in for the New Architecture, we load the native entry point for this app.
-      DefaultNewArchitectureEntryPoint.load();
+      load()
     }
-    ReactNativeFlipper.initializeFlipper(this, getReactNativeHost().getReactInstanceManager());
   }
 }
